7ef84a609f1b0b3e339260490e8d81af74ef2a7e
UnisLTU <109076072+UnisLTU@users.noreply.github.com>
2022-08-30 21:34:00 +0300

    Cocktails without navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import axios from "axios";
+import React, { useCallback, useEffect, useState } from "react";
 
-function App() {
+const App = () => {
+  const [data, SetData] = useState([]);
+  const [ingridientName, setIngridientName] = useState([]);
+  const [ingridientAmount, setIngridientAmount] = useState([]);
+  const [flip, setFlip] = useState(false);
+
+  const getDrinks = useCallback(() => {
+    const req = async () => {
+      const response = await axios.get(
+        "https://www.thecocktaildb.com/api/json/v1/1/random.php"
+      );
+      const drinks = response.data.drinks[0];
+      SetData(drinks);
+      setFlip(false);
+      setIngridientName(
+        Object.values(
+          Object.fromEntries(
+            Object.entries(drinks).filter(([key]) =>
+              key.includes("strIngredient")
+            )
+          )
+        )
+      );
+      setIngridientAmount(
+        Object.values(
+          Object.fromEntries(
+            Object.entries(drinks).filter(([key]) => key.includes("strMeasure"))
+          )
+        )
+      );
+    };
+    req();
+  }, []);
+
+  useEffect(() => {
+    getDrinks();
+  }, [getDrinks]);
+
+  const MapIndri = 
+    ingridientName.map((name, i) => (
+      <li className="ingridient_name" key={i}>
+        {name}
+      </li>
+    ));
+  
+
+  const MapAmount = 
+    ingridientAmount.map((name, i) => (
+      <li className="ingridient_amount" key={i}>
+        {name}
+      </li>
+    ));
+
+  console.log(data);
+
   return (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div
+        className={`card ${flip ? "flip" : ""}`}
+        onClick={() => setFlip(!flip)}
+      >
+        <div className="front">
+          <h2>{data.strDrink}</h2>
+          <h3>Category: {data.strCategory}</h3>
+          <img src={data.strDrinkThumb} alt="" />
+        </div>
+        <div className="back">
+          <h3>Served in : {data.strGlass}</h3>
+          <h3>
+            Ingridients :
+            <div className="ingridients">
+              <ul className="ingridient_name_container">{MapAmount}</ul>
+              <ul className="ingridient_name_container">{MapIndri}</ul>
+            </div>
+            Instructions : <p>{data.strInstructions}</p>
+          </h3>
+        </div>
+      </div>
+      <button onClick={getDrinks}>Get new drink</button>
     </div>
   );
-}
+};
 
 export default App;
